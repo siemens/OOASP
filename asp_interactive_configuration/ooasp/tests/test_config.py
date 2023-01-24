b9c199a6efb139fdaf9482506995f519107548af
gschennersie <19549657+gschennersie@users.noreply.github.com>
2023-01-24 15:01:00 +0100

    Update asp_interactive_configuration/ooasp/tests/test_config.py
    Co-authored-by: Richard Comploi-Taupe <rtaupe@edu.aau.at>

--- asp_interactive_configuration/ooasp/tests/test_config.py
+++ asp_interactive_configuration/ooasp/tests/test_config.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2022 Siemens AG Oesterreich
 # SPDX-License-Identifier: MIT
 
-from clorm.clingo import Control
 from ooasp.kb import OOASPKnowledgeBase
 from ooasp.config import OOASPConfiguration
 
