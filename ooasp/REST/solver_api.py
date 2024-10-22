4fff411e6c627325206d9e73d3dc4ce7afaf38ba
Balazova, Lucia (T DAI CON-AT) <balazova.lucia@siemens.com>
2024-10-22 11:27:41 +0200

    Implemented knowledgebase loading into REST

--- ooasp/REST/solver_api.py
+++ ooasp/REST/solver_api.py
@@ -6,20 +6,59 @@ from typing import List
 
 import os
 
-global solver, setup_flag, st
+global solver, setup_flag, st, allowed_objects, allowed_associations
 
 SMART_FUNCTIONS = ["association_possible", "assoc_needs_object", "global_lb_gap", "global_ub_gap"]
 
 solver = SmartOOASPSolver(smart_generation_functions=SMART_FUNCTIONS)
 setup_flag = False
 st = ""
+allowed_objects = []
+allowed_associations = {}
 app = FastAPI()
 
+def load_known_names(kb_path):
+    global allowed_objects
+    allowed_objects = []
+    with open(kb_path, "r+") as f:
+        facts = f.read().strip().replace("\n","").replace("\r","").split(".")
+        for fact in facts:
+            if "%" in fact:
+                continue
+            if "ooasp_class" in fact:
+                class_name = fact.split("(")[1][0:-1] 
+                allowed_objects.append(class_name)
+    return Response("Allowed Names.", data=allowed_objects)
+
+def load_known_associations(kb_path):
+    global allowed_associations
+    allowed_associations = {}
+    with open(kb_path, "r+") as f:
+        facts = f.read().strip().replace("\n","").replace("\r","").split(".")
+        for fact in facts:
+            if "%" in fact:
+                continue
+            if "ooasp_assoc(" in fact:
+                contents = fact.split("(")
+                assoc_info = contents[1].replace(")","").split(",")
+                print(assoc_info)
+                allowed_associations.update({
+                    assoc_info[0]: {
+                        "from": assoc_info[1],
+                        "fromMin": assoc_info[2],
+                        "fromMax": assoc_info[3],
+                        "to": assoc_info[4],
+                        "toMin": assoc_info[5],
+                        "toMax": assoc_info[6]
+                    }
+                })
+    return Response("Allowed Associations.", data=allowed_associations)
+
 def initialise_solver(solver, data):
-    data.objects = data.objects.split(",")
+    object_list = data.objects.split(",") if data.objects != "" else []
     data.prio_associations = data.prio_associations.split(",")
     solver.ctl.load(data.domain)
-    solver.initial_objects =  data.objects
+    solver.initial_objects =  object_list
     solver.associations_with_priority = data.prio_associations
 
     solver.load_base()
@@ -28,6 +67,8 @@ def initialise_solver(solver, data):
     global setup_flag
     setup_flag = True
 
+    load_known_associations(data.domain)
+    load_known_names(data.domain)
     return Response(message="Solver was initialised.", data=solver.__dict__).build()
 
 def filter_model():
@@ -64,6 +105,18 @@ class InitData(BaseModel):
     prio_associations : str = ""
     domain : str = str(os.path.join("examples", "racks", "kb.lp"))
 
+@app.post("/knowledgebase/{path}")
+async def read_kb(path):
+    path =  os.path.join(*path.split("-"))
+    assoc = load_known_associations(path)
+    cls = load_known_names(path)
+    return {"classes": assoc, "associations":cls}
+
+@app.get("/knowledgebase")
+async def show_loaded_kb():
+    global allowed_associations, allowed_objects
+    return Response("Known class names and associations.", {"classes": allowed_objects, "associations": allowed_associations})
+
 @app.get("/test")
 async def add():
     global st
@@ -78,7 +131,7 @@ async def activity():
 async def init_solver(values: InitData):
     return initialise_solver(solver, values)
 
-@app.post("/add/{cls}")
+@app.put("/add/{cls}")
 async def add_object(cls):
     global solver
     solver.add_object(str(cls))
@@ -113,10 +166,14 @@ async def associate(id1, id2, name):
     """
     Creates an association between two objects.
     """
+    global allowed_associations
+    if name not in allowed_associations.keys():
+        return Response("This association is not defined in the domain's knowledgebase.", data=allowed_associations).build(code=status.HTTP_400_BAD_REQUEST)
+
     global solver
     solver.associate((name,int(id1), int(id2)))
     succ =  f"ooasp_associated({name},{id1},{id2})" in solver.assumptions
-    return Response("Succesfully added." if succ else "Error while adding.",data=solver.assumption_list).build(code=status.HTTP_200_OK if succ else status.HTTP)
+    return Response("Succesfully added." if succ else "Error while adding.",data=solver.assumption_list).build(code=status.HTTP_200_OK if succ else status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @app.get("/consequences/cautious")
 async def get_cautious():
@@ -124,15 +181,19 @@ async def get_cautious():
     Returns cautions consequences.
     These are things true in all models, meaning that these steps must be taken in order to get the result
     """
-    pass
+    global solver
+    csq = str(solver.get_cautious()).split(",")
+    return Response("Brave consequences (must haves)", str(csq))
 
-@app.get("consequences/brave")
+@app.get("/consequences/brave")
 async def get_brave():
     """
     Returns brave consequences.
     These are things true in at least one model, meaning that these are suggestions.
     """
-    pass
+    global solver
+    csq = str(solver.get_brave()).split(",")
+    return Response("Brave consequences (possibilities)", str(csq))
 
 
 @app.get("/solution/view/{name}")
